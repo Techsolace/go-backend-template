e019b23de5905b6a6dd3f46bf51854f9d42c0c69
samarthwtf <samarthplayz1337@gmail.com>
2024-06-10 10:40:58 +0530

    added prisma configuration

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module techsolace
 
 go 1.22.3
 
-require github.com/steebchen/prisma-client-go v0.37.0 // indirect
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/shopspring/decimal v1.4.0
+	github.com/steebchen/prisma-client-go v0.37.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"techsolace/prisma/db"
+)
 
 func main() {
-	fmt.Printf("Hello World")
+	if err := Run(); err != nil {
+		panic(err)
+	}
+}
+
+func Run() error {
+	client := db.NewClient()
+	if err := client.Prisma.Connect(); err != nil {
+		return err
+	}
+	ctx := context.Background()
+
+	addUser, err := client.User.CreateOne(
+		db.User.Mail.Set("samarth1234"),
+		db.User.Name.Set("Samarth"),
+	).Exec(ctx)
+	if err != nil {
+		return err
+	}
+	result, _ := json.MarshalIndent(addUser, "", " ")
+	fmt.Printf("added user %v", result)
+	return nil
 }
