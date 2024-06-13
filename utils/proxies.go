26c92271a124f12ed65cd17b6f8cd98909ed80d6
samarthwtf <samarthplayz1337@gmail.com>
2024-06-13 06:09:56 +0530

    made minor changes

--- utils/proxies.go
+++ utils/proxies.go
@@ -7,7 +7,7 @@ import (
 )
 
 func SetTrustedProxies(router *gin.Engine) {
-	proxies := []string{"0.0.0.0"}
+	proxies := []string{"103.215.227.93"}
 	if err := router.SetTrustedProxies(proxies); err != nil {
 		log.Fatalf("Error Setting Trusted proxies %v", err)
 	}
